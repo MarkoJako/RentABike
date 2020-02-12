7efb8c14ec49e77d5a54dbc9a14875fcddbe0ac1
unknown <marko.jakovljevic985@gmail.com>
2020-02-12 12:35:33 +0100

    JS code moved to JS file

---
+++ RentABike/Scripts/JS/bicycleIndex.js
@@ -0,0 +1,9 @@
+﻿
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('table .delete').on('click', function () {
+        var id = $(this).parent().find('#id').val();
+        $('#deleteModal #id').val(id);
+    });
+});
---
+++ RentABike/Scripts/JS/home.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $(".content").hide();
+    $(".button").on("click", function () {
+        var txt = $(".content").is(':visible') ? 'Read More' : 'Read Less';
+        $(this).text(txt);
+        $(this).prev("div").slideToggle(200);
+    });
+});
